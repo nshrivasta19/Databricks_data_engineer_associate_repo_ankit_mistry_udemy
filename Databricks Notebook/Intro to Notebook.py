d236cb9d3238d837f4060e8a125b32bbdd5c5923
nshrivasta19 <nshrivasta19>
2024-05-01 10:24:15 +0000

    new file

---
+++ Databricks Notebook/Intro to Notebook.py
@@ -0,0 +1,58 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC #Databricks Notebooks
+
+# COMMAND ----------
+
+message = "Hi.. I am nikita and I am starting my journey to become a data engineer."
+
+# COMMAND ----------
+
+print(message)
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ##Magic commands
+# MAGIC The magic commands helps you document your code.
+# MAGIC
+# MAGIC - %python
+# MAGIC - %sql
+# MAGIC - % scala
+# MAGIC - %r
+# MAGIC - %md
+# MAGIC cheat sheet - https://www.markdownguide.org/cheat-sheet/#basic-synta
+
+# COMMAND ----------
+
+message = "Hello Databricks Notebook"
+
+# COMMAND ----------
+
+SELECT "databricks"
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC SELECT "databricks"
+
+# COMMAND ----------
+
+# MAGIC %scala
+# MAGIC val msg = "Databricks Magic Commands"
+# MAGIC print(msg)
+# MAGIC
+
+# COMMAND ----------
+
+# MAGIC %fs
+# MAGIC ls
+
+# COMMAND ----------
+
+# MAGIC %sh
+# MAGIC ps
+
+# COMMAND ----------
+
+
